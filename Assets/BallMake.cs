ea20ed7d55868757598ac855d9f8b7296018f172
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-23 07:56:50 +0330

    will make an player on game init

--- Assets/BallMake.cs
+++ Assets/BallMake.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BallMake : MonoBehaviour
 {
+    public GameObject Player;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(Player);
     }
 
     // Update is called once per frame
