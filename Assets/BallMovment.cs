1a91062e534344d4a7ffa8729e3f170c0901dffd
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-23 08:57:18 +0330

    playerMovment

--- Assets/BallMovment.cs
+++ Assets/BallMovment.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallMovment : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class BallMovment : MonoBehaviour {
+
+    void Start () {
+
+            Debug.Log("Hi");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        if (Input.GetKey (KeyCode.UpArrow)) {
+            Debug.Log("MOVE BITCH");
+            transform.position += new Vector3(0,30,0) * Time.deltaTime;
+        }
+        else if (Input.GetKey (KeyCode.DownArrow)) {
+            Debug.Log("MOVE BITCH");
+            transform.position += new Vector3(0,-30,0) * Time.deltaTime;
+        }
+        else if (Input.GetKey (KeyCode.RightArrow)) {
+            Debug.Log("MOVE BITCH");
+            transform.position += new Vector3(30,0,0) * Time.deltaTime;
+        }
+        else if (Input.GetKey (KeyCode.LeftArrow)) {
+            Debug.Log("MOVE BITCH");
+            transform.position += new Vector3(-30,0,0) * Time.deltaTime;
+        }
     }
 }
