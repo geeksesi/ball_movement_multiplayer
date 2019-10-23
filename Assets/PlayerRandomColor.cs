8683dddcfa9dcdf66836b4a6d33e3f6460f544c4
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-23 10:42:02 +0330

    now we have random color for player

--- Assets/PlayerRandomColor.cs
+++ Assets/PlayerRandomColor.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomColor : MonoBehaviour
-{
+public class PlayerRandomColor : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start () {
+        Debug.Log (Random.value);
+        Color newColor = new Color (Random.value, Random.value, Random.value, 1.0f);
+        // apply it on current object's material
+        gameObject.GetComponent<Renderer> ().material.color = newColor;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 }
