d0ac9e6d21a5fa2d31970d5a6bb8fcac4178e31b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-28 15:39:47 +0330

    make ball object

---
+++ Assets/BallObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class BallObject : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The speed that the player will move.")]
+    float speed = 1f;
+
+    Vector3 movePosition;
+
+    void Awake()
+    {
+        movePosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (speed != 0f)
+            transform.position = Vector3.MoveTowards(transform.position, movePosition, speed * Time.deltaTime);
+    }
+
+    internal void SetColor(Color32 color)
+    {
+        Renderer renderer = GetComponent<Renderer>();
+
+        renderer.material.color = color;
+    }
+
+    internal void SetMovePosition(Vector3 newPosition)
+    {
+        movePosition = newPosition;
+    }
+}
