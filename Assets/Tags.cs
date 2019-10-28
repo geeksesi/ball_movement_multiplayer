84467066bb47421da422d28471e6a63a2cfc0358
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-28 15:39:29 +0330

    copy server side tag here

---
+++ Assets/Tags.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+static class Tags {
+    public static readonly ushort SpawnPlayerTag = 0;
+    public static readonly ushort MovePlayerTag = 1;
+    public static readonly ushort DespawnPlayerTag = 2;
+
+
+}
