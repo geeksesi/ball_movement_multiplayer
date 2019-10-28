d3680fc0c8e6805c165f05531ec2e9274a593b97
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-28 15:41:57 +0330

    ball movement by mouse ( copy from darkrift agar.io )

--- Assets/BallMovment.cs
+++ Assets/BallMovment.cs
@@ -1,27 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DarkRift;
+using DarkRift.Client;
 using UnityEngine;
 
 public class BallMovment : MonoBehaviour {
+    BallObject BallObject;
+
+    void Awake () {
+        BallObject = GetComponent<BallObject> ();
+    }
 
     void Start () {
 
-            Debug.Log("Hi");
+        Debug.Log ("Hi");
     }
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKey (KeyCode.UpArrow)) {
-            transform.position += new Vector3(0,15,0) * Time.deltaTime;
-        }
-        else if (Input.GetKey (KeyCode.DownArrow)) {
-            transform.position += new Vector3(0,-15,0) * Time.deltaTime;
-        }
-        else if (Input.GetKey (KeyCode.RightArrow)) {
-            transform.position += new Vector3(15,0,0) * Time.deltaTime;
-        }
-        else if (Input.GetKey (KeyCode.LeftArrow)) {
-            transform.position += new Vector3(-15,0,0) * Time.deltaTime;
-        }
+        Vector3 mousePoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        mousePoint.z = 0;
+
+        BallObject.SetMovePosition (mousePoint);
+        // if (Input.GetKey (KeyCode.UpArrow)) {
+        //     transform.position += new Vector3(0,15,0) * Time.deltaTime;
+        // }
+        // else if (Input.GetKey (KeyCode.DownArrow)) {
+        //     transform.position += new Vector3(0,-15,0) * Time.deltaTime;
+        // }
+        // else if (Input.GetKey (KeyCode.RightArrow)) {
+        //     transform.position += new Vector3(15,0,0) * Time.deltaTime;
+        // }
+        // else if (Input.GetKey (KeyCode.LeftArrow)) {
+        //     transform.position += new Vector3(-15,0,0) * Time.deltaTime;
+        // }
     }
 }
