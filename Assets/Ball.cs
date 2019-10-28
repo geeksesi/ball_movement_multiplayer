217797ce705aeed5f5f390efe1cd3d3483bd59a3
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-10-28 15:42:14 +0330

    send ball status to server

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DarkRift;
+using DarkRift.Client;
+using DarkRift.Client.Unity;
 using UnityEngine;
+public class Ball : MonoBehaviour {
+    const byte MOVEMENT_TAG = 1;
 
-public class Ball : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    [Tooltip ("The distance we can move before we send a position update.")]
+    float moveDistance = 0.05f;
+
+    public UnityClient Client { get; set; }
+
+    Vector3 lastPosition;
+
+    void Awake () {
+        lastPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        if (Vector3.Distance (lastPosition, transform.position) > moveDistance) {
+            /* Send position to server here */
+            lastPosition = transform.position;
+            using (DarkRiftWriter writer = DarkRiftWriter.Create ()) {
+                writer.Write (transform.position.x);
+                writer.Write (transform.position.y);
+
+                using (Message message = Message.Create (Tags.MovePlayerTag, writer))
+                Client.SendMessage (message, SendMode.Unreliable);
+            }
+        }
     }
 }
